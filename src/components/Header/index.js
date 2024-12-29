f858e91f0dc092b96afaa605d793341833367535
Julian Basco <bascojulian31@gmail.com>
2024-12-29 18:47:58 +0800

    Charm edit the header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -13,12 +13,12 @@ const Header = () => {
             <div className="header">
                 <div className="container">
                     <div className="row">
-                        <div className="col-lg-3 col-10">
+                        <div className="col-lg-4 col-10">
                             <div className="logo">
-                                <h2><Link to='/home'>Julian<span><i className="fa fa-heart" aria-hidden="true"></i></span>Charm</Link></h2>
+                                <h2><Link to='/home'>Julian<span><i className="fa fa-heart" aria-hidden="true"></i></span>Charmaine</Link></h2>
                             </div>
                         </div>
-                        <div className="col-lg-9">
+                        <div className="col-lg-8">
                             <div className="header-menu d-lg-block d-none">
                                 <ul className="mobail-menu d-flex">
                                     <li><Link to='#'>Home</Link>
