7ff02c36692858ac398ba64e7f778f3b827d92b9
Juls <bascojulian31@gmail.com>
2025-01-08 08:25:59 +0800

    Remove desktop home link

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -14,21 +14,22 @@ const Header = () => {
             <div className="header">
                 <div className="container">
                     <div className="row align-items-center">
-                        <div className="col-lg-4 col-10">
+                        {/* <div className="col-lg-4 col-10">
+                            
                             <div className="logo">
                                 <h2><Link to='/home'><img className="logo-image" src={JCLogo} /> Julian and Charmaine</Link></h2>
                             </div>
-                        </div>
+                        </div> */}
                         <div className="col-lg-8">
                             <div className="header-menu d-lg-block d-none">
                                 <ul className="mobail-menu d-flex">
-                                    <li><Link to='#'>Home</Link>
-                                        <ul className="submenu">
+                                    <li><Link to='/'>Home</Link>
+                                        {/* <ul className="submenu">
                                             <li><Link to='/'>Home Style 1</Link></li>
                                             <li><Link to='/home2'>Home Style 2</Link></li>
                                             <li><Link to='/home3'>Home Style 3</Link></li>
                                             <li><Link to='/home4'>Home Style 4</Link></li>
-                                        </ul>
+                                        </ul> */}
                                     </li>
                                     <li><AnchorLink href='#couple'>Couple</AnchorLink></li>
                                     <li><AnchorLink href='#story'>Story</AnchorLink></li>
